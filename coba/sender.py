7a2ee9910e9532b7c615ede71df3f2e8cb3ba36b
nabilahrobbaniyah <255456781+nabilahrobbaniyah@users.noreply.github.com>
2026-05-24 18:48:38 +0700

    Refactor multicast sender to simplify file sending

--- coba/sender.py
+++ coba/sender.py
@@ -1,93 +1,26 @@
-# multicast
 import socket
 import struct
-import os
 
-# ==========================
-# KONFIGURASI
-# ==========================
+GROUP="239.255.1.1"
+PORT=5000
 
-MULTICAST_GROUP = "224.1.1.1"
-PORT = 5000
-
-# GANTI DENGAN IP LAPTOP SENDER
-LOCAL_IP = "192.168.1.10"
-
-# ==========================
-# BUAT SOCKET UDP
-# ==========================
-
-sock = socket.socket(
+sock=socket.socket(
     socket.AF_INET,
-    socket.SOCK_DGRAM
+    socket.SOCK_DGRAM,
+    socket.IPPROTO_UDP
 )
-
-# TTL = hanya LAN
-ttl = struct.pack("b", 1)
 
 sock.setsockopt(
     socket.IPPROTO_IP,
     socket.IP_MULTICAST_TTL,
-    ttl
+    struct.pack("b",2)
 )
 
-# Tentukan interface jaringan
-sock.setsockopt(
-    socket.IPPROTO_IP,
-    socket.IP_MULTICAST_IF,
-    socket.inet_aton(LOCAL_IP)
-)
+while True:
 
-print("=== MULTICAST SENDER ===")
+    msg=input()
 
-# ==========================
-# INPUT FILE
-# ==========================
-
-filepath = input(
-    "Masukkan path file: "
-)
-
-filename = os.path.basename(filepath)
-
-# ==========================
-# KIRIM NAMA FILE
-# ==========================
-
-sock.sendto(
-    f"FILENAME:{filename}".encode(),
-    (MULTICAST_GROUP, PORT)
-)
-
-print("Nama file dikirim")
-
-# ==========================
-# KIRIM ISI FILE
-# ==========================
-
-with open(filepath, "rb") as file:
-
-    while True:
-
-        data = file.read(1024)
-
-        if not data:
-            break
-
-        sock.sendto(
-            data,
-            (MULTICAST_GROUP, PORT)
-        )
-
-# ==========================
-# KIRIM PENANDA SELESAI
-# ==========================
-
-sock.sendto(
-    b"END_FILE",
-    (MULTICAST_GROUP, PORT)
-)
-
-print("File berhasil dikirim")
-
-sock.close()
+    sock.sendto(
+        msg.encode(),
+        (GROUP,PORT)
+    )
