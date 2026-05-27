c815133e3646c22c7254a9ca70f2d13383473fd4
nabilahrobbaniyah <255456781+nabilahrobbaniyah@users.noreply.github.com>
2026-05-27 16:37:57 +0700

    Enhance file sending functionality in senderm.py
    Refactor file sending logic to read and send file data in chunks, and prompt user for file path.

--- coba/senderm.py
+++ coba/senderm.py
@@ -1,12 +1,12 @@
 import socket
 import struct
+import os
 
-GROUP="239.255.1.1"
-PORT=5000
+GROUP = "239.255.1.1"
+PORT = 5000
+BUFFER = 1024
 
-LOCAL_IP="192.168.137.100"
-
-sock=socket.socket(
+sock = socket.socket(
     socket.AF_INET,
     socket.SOCK_DGRAM,
     socket.IPPROTO_UDP
@@ -15,20 +15,44 @@ sock=socket.socket(
 sock.setsockopt(
     socket.IPPROTO_IP,
     socket.IP_MULTICAST_TTL,
-    struct.pack("b",2)
+    struct.pack("b", 2)
 )
 
-sock.setsockopt(
-    socket.IPPROTO_IP,
-    socket.IP_MULTICAST_IF,
-    socket.inet_aton(LOCAL_IP)
+path = input(
+    "Masukkan path file: "
 )
 
-while True:
+filename = os.path.basename(
+    path
+)
 
-    msg=input()
+sock.sendto(
+    f"FILE:{filename}".encode(),
+    (GROUP, PORT)
+)
 
-    sock.sendto(
-        msg.encode(),
-        (GROUP,PORT)
-    )
+with open(
+    path,
+    "rb"
+) as file:
+
+    while True:
+
+        data = file.read(
+            BUFFER
+        )
+
+        if not data:
+            break
+
+        sock.sendto(
+            data,
+            (GROUP, PORT)
+        )
+
+sock.sendto(
+    b"EOF",
+    (GROUP, PORT)
+)
+
+print("File berhasil dikirim")
